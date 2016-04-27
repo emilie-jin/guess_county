7fb0dc11f1d243d3cde5dfeb432552dccc253f8b
emilie-jin <emilie.jin@hotmail.com>
2016-04-27 19:02:23 +0200

    English subjects

--- guess_county/src/score/Question.java
+++ guess_county/src/score/Question.java
@@ -57,31 +57,31 @@ public class Question {
                 this.setQuestionDegre(0);
                 this.setQuestion("Is the country we're looking for part of " + parameter + "?");
                 break;
-            case "Nombre de langues":
+            case "NumberOfLanguages":
                 this.setQuestionDegre(1);
                 this.setQuestion("Are more than " + parameter + " language(s) spoken in your country?");
                 break;
-            case "Nombre d'habitants":
+            case "AmountOfPeople":
                 this.setQuestionDegre(3);
                 this.setQuestion("Does your country host more than " + parameter + " people?");
                 break;
-            case "Superficie":
+            case "Area":
                 this.setQuestionDegre(3);
                 this.setQuestion("Is the country you're thinking of bigger than " + parameter + " m²?");
                 break;
-            case "Ocean/Mer":
+            case "Ocean/Sea":
                 this.setQuestionDegre(4);
                 this.setQuestion("Does the " + parameter + " bord the country searched?");
                 break;
-            case "Monnaie":
+            case "Currently":
                 this.setQuestionDegre(9);
                 this.setQuestion("Is the " + parameter + " the official currency of your country?");
                 break;
-            case "Langues officielles":
+            case "OfficialLanguages":
                 this.setQuestionDegre(8);
                 this.setQuestion("Is " + parameter + " an official language in the country you're thinking of?");
                 break;
-            case "Sport national":
+            case "NationalSports":
                 this.setQuestionDegre(7);
                 this.setQuestion("Is " + parameter + " the national sport?");
                 break;
@@ -89,11 +89,11 @@ public class Question {
                 this.setQuestionDegre(5);
                 this.setQuestion("Is " + parameter + " a major religion followed in the country you're thinking of?");
                 break;
-            case "Nombre de frontières":
+            case "NumberOfBoundaries":
                 this.setQuestionDegre(2);
                 this.setQuestion("Does your country have more than " + parameter + " boundaries?");
                 break;
-            case "Politique":
+            case "PoliticalRegime":
                 this.setQuestionDegre(6);
                 this.setQuestion("Is the country seeked a " + parameter + "?");
                 break;
